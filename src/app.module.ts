23ec1a88be7ff18732d362ef94066df36ead21b0
Daler <isoev9733@gmail.com>
2025-12-01 10:21:04 +0400

    Подключил и настроил Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { DatabaseModule } from './database/database.module';
       envFilePath: '.env.dev',
     }),
     DatabaseModule,
+    RedisModule,
   ],
 })
 export class AppModule {}
---
+++ src/redis/redis.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        isGlobal: true,
+        store: redisStore,
+        host: configService.get('REDIS_HOST', 'localhost'),
+        port: configService.get('REDIS_PORT', 6379),
+        ttl: configService.get('REDIS_TTL', 3600),
+        password: configService.get('REDIS_PASSWORD'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class RedisModule {}
